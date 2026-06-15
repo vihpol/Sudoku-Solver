7a879e306a33cd6ebc549ad1617bb1193a4ea005
vihpol <vihaanp2007@gmail.com>
2026-06-15 16:02:39 -0700

    added a way to save the puzzle into a 2d array

---
+++ sudoku.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class sudoku {
+
+    static int[][] puzzle = new int[9][9];
+
+    public sudoku (int[][] puzzle){
+        sudoku.puzzle = puzzle;
+    }
+
+    public static void readFromTxt() {
+        try (BufferedReader br = new BufferedReader(new FileReader("sudoku.txt"))) {
+            String line;
+            int count = 0;
+            while ((line = br.readLine()) != null) {
+                String stripped = line.replace(" ", "");
+                
+                int number = 0;
+                
+                for (int i = 0; i < stripped.length(); i++ ) {
+                    number = Integer.parseInt(String.valueOf(stripped.charAt(i)));
+                    puzzle[count][i] = number;
+                    // System.out.print(puzzle[count][i]);
+                    
+                   
+    
+                    
+                }
+                count++;
+                
+               
+            }
+        } catch (IOException e) {
+            System.out.println("Error Reading File");
+           
+        }
+
+    }
+
+    public static void printPuzzle() {
+        for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[i].length; j++) {
+                System.out.print(puzzle[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+        readFromTxt();
+        printPuzzle();
+    }
+    
+
+
+}
